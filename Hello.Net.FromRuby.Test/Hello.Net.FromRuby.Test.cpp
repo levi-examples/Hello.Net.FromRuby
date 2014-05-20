1367620ba0b08046de9ae2800a8db220ae020d35
Levi Wilson <levi@leviwilson.com>
2014-05-20 01:25:33 -0400

    failing test for Person cleanup

--- Hello.Net.FromRuby.Test/Hello.Net.FromRuby.Test.cpp
+++ Hello.Net.FromRuby.Test/Hello.Net.FromRuby.Test.cpp
@@ -1,11 +1,15 @@
 #include "stdafx.h"
+#include <Person.h>
+#include "MemoryLeakDetector.h"
 
-TEST(Hello, GoogleTest) {
+TEST(Person, ItCleansUpMemory) {
+  auto person = Person(true, 123, "The name", "English");
 }
 
 int main(int argc, char* argv[])
 {
   ::testing::InitGoogleTest(&argc, argv);
+  UnitTest::GetInstance()->listeners().Append(new MemoryLeakDetector());
   return RUN_ALL_TESTS();
 }
 
